3b23e6835764001b935168d927e314c19ad92f20
Balvania <bernadette@sapfira.com>
2024-06-11 20:23:31 +0200

    Added new directry for part I, II, and III
    Copied code into part 1 and renamed files to lesson4_part_i.html, part_i.css, and part_i.js

---
+++ lessons/lesson04/part_i/part_i.js
@@ -0,0 +1,28 @@
+let home_long = 29.0000;
+let home_lat = -15.4167;
+ 
+require(["esri/Map", "esri/views/MapView", "esri/geometry/Point", "esri/symbols/SimpleMarkerSymbol", "esri/Graphic"], (Map, MapView, Point, SimpleMarkerSymbol, Graphic) => {
+        const map = new Map({
+          basemap: "topo-vector"
+        });
+        const view = new MapView({
+          container: "viewDiv", // Reference to the view div created in step 5
+          map: map, // Reference to the map object created before the view
+          zoom: 4, // Sets zoom level based on level of detail (LOD)
+          center: [home_long, home_lat] // Sets center point of view using longitude,latitude
+        });
+        const pt = new Point({
+          latitude: home_lat,
+          longitude: home_long
+        });
+        const sym = new SimpleMarkerSymbol({
+          color: "blue",
+          style: "square",
+          size: 12
+        });
+        const ptGraphic = new Graphic({
+          geometry:pt,
+          symbol:sym
+        });
+        view.graphics.add(ptGraphic);
+      });
