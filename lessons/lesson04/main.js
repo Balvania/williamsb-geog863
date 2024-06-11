8e9ed2df0784ff68ecead25e3c24d49ba7d1f6bb
Balvania <bernadette@sapfira.com>
2024-06-11 20:00:11 +0200

    Update main.js
    Add lat/long for Lusaka
    Created home_lat and home_long variables

--- lessons/lesson04/main.js
+++ lessons/lesson04/main.js
@@ -1,5 +1,7 @@
-
- require(["esri/Map", "esri/views/MapView", "esri/geometry/Point", "esri/symbols/SimpleMarkerSymbol", "esri/Graphic"], (Map, MapView, Point, SimpleMarkerSymbol, Graphic) => {
+let home_long = 29.0000;
+let home_lat = -15.4167;
+ 
+require(["esri/Map", "esri/views/MapView", "esri/geometry/Point", "esri/symbols/SimpleMarkerSymbol", "esri/Graphic"], (Map, MapView, Point, SimpleMarkerSymbol, Graphic) => {
         const map = new Map({
           basemap: "topo-vector"
         });
@@ -7,11 +9,11 @@
           container: "viewDiv", // Reference to the view div created in step 5
           map: map, // Reference to the map object created before the view
           zoom: 4, // Sets zoom level based on level of detail (LOD)
-          center: [-95, 40] // Sets center point of view using longitude,latitude
+          center: [home_long, home_lat] // Sets center point of view using longitude,latitude
         });
         const pt = new Point({
-          latitude: 40.792,
-          longitude: -77.871
+          latitude: home_lat,
+          longitude: home_long
         });
         const sym = new SimpleMarkerSymbol({
           color: "blue",
