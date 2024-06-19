68fc574fd0d5e3d5635410928be1fdc3929db28c
Balvania <bernadette@sapfira.com>
2024-06-19 19:07:07 +0200

    Update lesson5_ii.js
    Updated code to display MapImageLayer, first attempt did not work

--- lessons/lesson05/part_ii/lesson5_ii.js
+++ lessons/lesson05/part_ii/lesson5_ii.js
@@ -2,24 +2,25 @@
 require(["esri/Map", "esri/views/MapView", "esri/layers/MapImageLayer"], (
         Map,
         MapView,
-        MapImageLayer
-      ) => {
+        MapImageLayer) => {
 
-      	// creates a MapImageLayer pointing to the USA map
-      	let layer = new MapImageLayer({
+      	// create a MapImageLayer instance pointing to the USA map service
+      	const layer = new MapImageLayer({
       		url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer"
       	})
 
-        // Create a Map
-        const map = new Map();
+        // create a map and add the layer
+        const map = new Map({
+        	basemap: "dark-gray-vector",
+        	layers: [layer]
+        });
 
         // Make map view and bind it to the map
         const view = new MapView({
           container: "viewDiv",
           map: map,
+          zoom: 4,
+          center: [-98.102, 34.501]
         });
-
-        // adds the layer to the map
-        map.add(layer);
 
       });
