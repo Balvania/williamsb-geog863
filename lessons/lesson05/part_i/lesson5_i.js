32b382103f1c0727da45b7d721f4eabb99ad606e
Balvania <bernadette@sapfira.com>
2024-06-19 16:53:01 +0200

    Lesson 5 i updates
    Split into .html, .css, and .js files

---
+++ lessons/lesson05/part_i/lesson5_i.js
@@ -0,0 +1,39 @@
+
+require(["esri/Map", "esri/views/SceneView", "esri/WebScene","esri/layers/VectorTileLayer"], (
+        Map,
+        SceneView,
+        WebScene,
+        VectorTileLayer
+      ) => {
+
+
+        // Create a Map
+        const map = new Map();
+
+
+        // Make 3D scene view and bind it to the map
+        // Center on Houston, Texas
+        const view = new SceneView({
+          container: "viewDiv",
+          map: map,
+          center: [-95.3656, 29.7607],
+          zoom: 10
+        });
+
+        /********************************************************************
+         * Add the TxDOT vector tile layer to the map
+         *
+         *********************************************************************/
+        const tileLayer = new VectorTileLayer({
+          url:
+            "https://tiles.arcgis.com/tiles/KTcxiTD9dsQw4r7Z/arcgis/rest/services/TxDOT_Vector_Tile_Basemap/VectorTileServer"
+        });
+        map.add(tileLayer);
+
+        view.when(function() {
+          // when the scene and view resolve, display the scene's
+          // title in the DOM
+          const title = "Assignment 5.1: Texas Department of Transportation Vector Tile Layer";
+          titleDiv.innerHTML = title;
+        });
+      });
