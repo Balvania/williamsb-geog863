7048bbbe72a4fcb6e5e7ffe2d4534cc624d964df
Balvania <bernadette@sapfira.com>
2024-06-19 20:09:38 +0200

    Update lesson5_ii.js
    Changed from footer to header
    Removed unnecessary code

--- lessons/lesson05/part_ii/lesson5_ii.js
+++ lessons/lesson05/part_ii/lesson5_ii.js
@@ -43,34 +43,4 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/MapImageLayer"], (
           zoom: 3
         });
 
-
-        /*****************************************************************
-         * Wait for Layer to load and update the page to refelect which
-         * layers are visible in the Map Service.
-         *****************************************************************
-        layer.when(() => {
-          layer.sublayers.map((sublayer) => {
-            const id = sublayer.id;
-            const visible = sublayer.visible;
-            const node = document.querySelector(".sublayers-item[data-id='" + id + "']");
-            if (visible) {
-              node.classList.add("visible-layer");
-            }
-          });
-        });
-
-        /*****************************************************************
-         * Listen for when buttons on the page have been clicked to turn
-         * layers on and off in the Map Service.
-         *****************************************************************
-        const sublayersElement = document.querySelector(".sublayers");
-        sublayersElement.addEventListener("click", (event) => {
-          const id = event.target.getAttribute("data-id");
-          if (id) {
-            const sublayer = layer.findSublayerById(parseInt(id));
-            const node = document.querySelector(".sublayers-item[data-id='" + id + "']");
-            sublayer.visible = !sublayer.visible;
-            node.classList.toggle("visible-layer");
-          }
-        }); */
       });
