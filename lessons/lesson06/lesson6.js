284ce4d0be42d221e35da5f50bc61caae5c82c81
Balvania <bernadette@sapfira.com>
2024-06-25 22:03:58 +0200

    Created Lesson 6
    New .css, .html, and .js for lesson 6 assignment - earthquakes

---
+++ lessons/lesson06/lesson6.js
@@ -0,0 +1,110 @@
+/**************************************************************************** 
+ * GEOG863 - Lesson 6 - Bernadette Williams
+ * Map of Earthquakes, a historical view 
+ * Created: 25 June 2024
+ ****************************************************************************/
+
+require([
+  "esri/Map",
+  "esri/views/SceneView",
+  "esri/Camera",
+  "esri/layers/FeatureLayer",
+  "esri/renderers/ClassBreaksRenderer",
+  "esri/symbols/PointSymbol3D",
+  "esri/symbols/IconSymbol3DLayer",
+  "esri/symbols/SimpleLineSymbol",
+  "esri/widgets/Legend"
+], (Map, SceneView, Camera, FeatureLayer, ClassBreaksRenderer, PointSymbol3D, IconSymbol3DLayer, SimpleLineSymbol, Legend) => {
+
+  const map = new Map({
+    basemap: "dark-gray-vector",
+    ground: "world-elevation"
+  });
+
+  const view = new SceneView({
+    container: "viewDiv",
+    map: map,
+    camera: new Camera({
+      position: [-74.5, 36.6, 229000],
+      heading: 328,
+      tilt: 64
+    })
+  });
+  
+  const quakeRenderer = new ClassBreaksRenderer({
+    field: "magnitude",
+    legendOptions: {
+      title: "Magnitude"
+    }
+  });
+
+  const addClass = function(min, max, clr, lbl, renderer) {
+    renderer.addClassBreakInfo({
+      minValue: min,
+      maxValue: max,
+      symbol: new PointSymbol3D({
+        symbolLayers: [new FillSymbol3DLayer({
+          material: {color: clr},
+          outline: {
+            color: "black",
+            size: 1
+          }
+        })]
+      }),
+      label: lbl
+    });
+  }
+
+/*
+  const countyRenderer = new ClassBreaksRenderer({
+    field: "POP1990",
+    normalizationField: "SQ_MILES",
+    legendOptions: {
+      title: "Pop per Sq Mile"   
+    }
+  });
+
+  const addClass = function(min, max, clr, lbl, renderer) {
+    renderer.addClassBreakInfo({
+      minValue: min,
+      maxValue: max,
+      symbol: new PolygonSymbol3D({
+        symbolLayers: [new FillSymbol3DLayer({
+          material: { color: clr },
+          outline: {   
+            color: "black",
+            size: 1
+          }
+        })]
+      }),
+      label: lbl
+    });      
+  }
+  */
+  
+  addClass(1.600000, 5.000000, "#eff3ff", "1.600000 - 5.000000", quakeRenderer);
+  addClass(5.000001, 5.900000, "#bdd7e7", "5.000001 - 5.900000", quakeRenderer);
+  addClass(5.900001, 6.900000, "#6baed6", "5.900001 - 6.900000", quakeRenderer);
+  addClass(6.900001, 9.000000, "#3182bd", "6.900001 - 9.000000", quakeRenderer);
+   
+  const quakeLayer = new FeatureLayer({
+    portalItem: { 
+      id: "7d68c73f72b24cb0af61a96cc9b8fb16"
+    },
+    renderer: quakeRenderer      
+  });  
+  
+  map.add(quakeLayer)
+  
+  const legend = new Legend({
+    view: view,
+    layerInfos: [{
+      layer: quakeLayer,
+      title: "Earthquakes: A Historical View"
+    }]
+  });
+
+  view.ui.add(legend, "bottom-left");  
+  
+});
+
