3a6fd9886a2616e54e70e369181a96fe3846a648
Balvania <bernadette@sapfira.com>
2024-07-02 20:00:56 +0200

    Update lesson7b.js
    added arcade expression for capturing % population of Gen X and being able to reuse that in the popup template. Fingers crossed.

--- lessons/lesson07/lesson7b.js
+++ lessons/lesson07/lesson7b.js
@@ -22,11 +22,46 @@ require([
     zoom: 6
   });
 
+/*Generational data fields:
+  OLDRGENSCY - Silent & Greatest Generations (born 1945 or earlier)
+  BABYBOOMCY - Baby Boomer (born 1946 to 1964)
+  GENX_CY - Generation X (born 1965 to 1980)
+  MILLENN_CY - Millennial (born 1981 to 1998)
+  GENZ_CY - Generation Z (born 1999 to 2016)
+  GENALPHACY - Generation Alpha (born 2017 or later)
+*/
+
+  const arcadeExpressionInfos = [
+  {
+    name: "%-gen-x-arcade",
+    title: "% of Gen X in county",
+    expression: "Round((($feature.GENX_CY/$feature.TOTPOP_CY)*100,2) + '%'"
+  }
+];
+
+
 // Create popup template
   const template = {
-    title: "{NAME}, {ST_ABBREV}: insert % here Gen X",
-    content: "Gen X represents % of the population in {NAME}, {ST_ABBREV}<br/>({GENX_CY} out of the county's total population {TOTPOP_CY})<br/><br/>Other generations:<br/>{OLDRGENSCY} Silent & Greatest Generation (born 1945 or earlier)<br/>{BABYBOOMCY} Baby Boomer (1946-1964)<br/>{MILLENN_CY} Millennial (born 1981-1998)<br/>{GENZ_CY} Gen Z (born 1999-2016)<br/> {GENALPHACY} Gen Alpha (born 2017 or later)",
-    fieldInfos: [{
+    title: "{NAME}, {ST_ABBREV}: {expression/%-gen-x-arcade}% Gen X",
+    content: [
+    {
+      type: "text",
+      text: 
+        "Gen X represents {expression/%-gen-x-arcade}% of the population in {NAME}, {ST_ABBREV}.<br/>" +
+        "({GENX_CY} of county's total population {TOTPOP_CY})<br/><br/>" +
+        "Other generations:<br/>{OLDRGENSCY} Silent & Greatest Generation (born 1945 or earlier)<br/>" +
+        "{BABYBOOMCY} Baby Boomer (1946-1964)<br/>{MILLENN_CY} Millennial (1981-1998)<br/>" +
+        "{GENZ_CY} Gen Z (1999-2016)<br/> {GENALPHACY} Gen Alpha (born 2017 or later)",
+    }],
+    fieldInfos: [
+    {
+      fieldName: "expression/%-gen-x-arcade",
+      format: {
+        digitSeparator: true,
+        places: 0
+      }
+    },
+    {
       fieldName: "GENX_CY",
       format: {
         digitSeparator: true
@@ -74,8 +109,10 @@ require([
   const countyRenderer = new ClassBreaksRenderer({
     field: "GENX_CY", // total Generation X (born 1965 to 1980)
     normalizationField: "TOTPOP_CY",  // total Population
+    valueExpression: "$feature.GENX_CY / $feature.TOTPOP_CY",
+    valueExpressionTitle: "where does this show Households per square mile",
     legendOptions: {
-      title: "Gen X % Population per County"   
+      title: "% Population (per 2018 Census)"   
     }
   });
 
@@ -96,11 +133,11 @@ require([
     })      
   };
   
-  addClass(0, 0.1, "#a6611a", "0-10%", countyRenderer);
-  addClass(0.1001, 0.2, "#d8b365", "10-20%", countyRenderer);
-  addClass(0.2001, 0.3, "#c7eae5", "20-30%", countyRenderer);
-  addClass(0.3001, 0.4, "#5ab4ac", "30-40%", countyRenderer);
-  addClass(0.4001, 1.0, "#01665e", "Over 40%", countyRenderer);
+  addClass(0, 0.05, "#a6611a", "< 5%", countyRenderer);
+  addClass(0.0501, 0.15, "#d8b365", "5-15%", countyRenderer);
+  addClass(0.1501, 0.25, "#c7eae5", "15-25%", countyRenderer);
+  addClass(0.2501, 0.35, "#5ab4ac", "25-35%", countyRenderer);
+  addClass(0.3501, 1.0, "#01665e", "Over 35%", countyRenderer);
 
 
   
@@ -113,13 +150,15 @@ require([
     popupTemplate: template
   });  
   
+  popupTemplate.expressionInfos = arcadeExpressionInfos;
+
   map.add(countyLyr)
   
   const legend = new Legend({
     view: view,
     layerInfos: [{
       layer: countyLyr,
-      title: "GenX Populations"
+      title: "Gen X Population by County"
     }]
   });
 
