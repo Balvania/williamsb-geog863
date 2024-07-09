0be8fa9f10adb8cab0a3db00dd5ad17f96b4e83b
Balvania <bernadette@sapfira.com>
2024-07-09 21:51:31 +0200

    lab 8
    added handler for clicks on action bar

--- labs/lesson08lab/lab8.js
+++ labs/lesson08lab/lab8.js
@@ -1,5 +1,6 @@
 
-  require(["esri/views/MapView", "esri/WebMap"], (MapView, WebMap) => {
+  require(["esri/views/MapView", "esri/WebMap", "esri/widgets/LayerList"], 
+  	(MapView, WebMap, LayerList) => {
     /************************************************************
      * Creates a new WebMap instance. A WebMap must reference
      * a PortalItem ID that represents a WebMap saved to
@@ -23,10 +24,41 @@
       container: "viewDiv"
     });
 
+    const layerList = new LayerList({ 
+        view: view,
+        selectionEnabled: true,
+        container: "layers-container"
+    });
+
     //dynamically set header
     webmap.when(() => {
     	const title = webmap.portalItem.title;
     	document.getElementById("header-title").textContent = title;
   	});
+
+  	let activeWidget;
+
+  	const handleActionBarClick = ( event ) => {
+	    const target = event.target; 
+	    if (target.tagName !== "CALCITE-ACTION") {
+	      return;
+	    }
+
+	    if (activeWidget) {
+	      document.querySelector(`[data-action-id=${activeWidget}]`).active = false;
+	      document.querySelector(`[data-panel-id=${activeWidget}]`).hidden = true;
+	    }
+
+	    const nextWidget = target.dataset.actionId;
+	    if (nextWidget !== activeWidget) {
+	      document.querySelector(`[data-action-id=${nextWidget}]`).active = true;
+	      document.querySelector(`[data-panel-id=${nextWidget}]`).hidden = false;
+	      activeWidget = nextWidget;
+	    } else {
+	      activeWidget = null;
+	    }
+	};
+
+	document.querySelector("calcite-action-bar").addEventListener("click", handleActionBarClick);
   });
 
