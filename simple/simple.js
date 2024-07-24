c1adaf24e2bed0d7a1194f08905a6e7f6863dfc2
Balvania <bernadette@sapfira.com>
2024-07-24 10:32:16 +0200

    Update simple.js
    updated ID to point to layer [0]

--- simple/simple.js
+++ simple/simple.js
@@ -37,7 +37,8 @@ require([
     portalItem: { 
       //id: "5af96a04ef4c4d8a9bb2a9dd2c883e36"
       //change to elephant layer
-      id: "4e1b261719ca421b9555ee6eb99bab8c"
+      //id: "4e1b261719ca421b9555ee6eb99bab8c"
+      id: "63256713fc3e4e26aefd2f21d342f64a" //using the updated layer ID of [0]
     },
     renderer: cityRenderer
   });
